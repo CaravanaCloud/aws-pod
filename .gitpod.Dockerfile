a56c21e8b5c201d82b8e2a16ba1d4e606ef719d1
Julio Faerman <jufaerma@redhat.com>
2022-07-30 13:37:42 +0000

    Updated perf test

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,8 +1,8 @@
-FROM gitpod/workspace-full
+FROM gitpod/workspace-mysql
 
 RUN bash -c ". /home/gitpod/.sdkman/bin/sdkman-init.sh && sdk install java 17.0.3.6.1-amzn && sdk default java 17.0.3.6.1-amzn && sdk install quarkus"
 RUN bash -c "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip' && unzip awscliv2.zip && sudo ./aws/install"
 RUN bash -c "npm install -g aws-cdk"
-RUN bash -c "brew install gh"
+RUN bash -c "brew install gh fio"
 RUN bash -c "sudo install-packages gettext mariadb-client tmux"
 
---
+++ snippets/fio-test-disk.sh
@@ -0,0 +1,40 @@
+Using the Flexible I/O Tester
+
+# Install FIO
+brew install fio
+
+https://linux.die.net/man/1/fio
+https://www.binarylane.com.au/support/solutions/articles/1000055889-how-to-benchmark-disk-i-o
+
+mkdir tmp;
+cd tmp;
+
+fio 
+  # File name and location 
+  --filename=/tmp/testfio \
+  # Name of the test
+  --name=fiotest \
+  # Size of the test
+  --size=16G \
+  # Read/Write distribution
+  --readwrite=randrw \
+  --rwmixread=75 \
+  # Block size (check https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/volume_constraints.html#block_size) 
+  --bs=4k \
+  
+  --iodepth=1 \
+  # Direct I/O to device (non-buffered)
+  --direct=1 \
+  # Reduce gettimeofday() calls
+  --gtod_reduce=1 \
+
+
+fio \
+  --filename=./testfio \
+  --name=fiotest \
+  --size=16G \
+  --readwrite=randrw \
+  --rwmixread=75 \
+  --bs=4k \
+  --direct=1 \
+  --gtod_reduce=1
