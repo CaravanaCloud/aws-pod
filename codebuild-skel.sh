dcac2c5b46d49e3eada3794c0f2f29b854e41eca
Julio Faerman <jufaerma@redhat.com>
2022-09-05 18:31:04 +0000

    change to t3.small db type

--- codebuild-skel.sh
+++ codebuild-skel.sh
@@ -1,2 +1,3 @@
 # aws codebuild create-project --generate-cli-skeleton > codebuild-skel.json 
 # aws codebuild create-project --cli-input-json file://codebuild-skel.json 
+# aws codebuild start-build --project-name multiverse-build-nopipeline
