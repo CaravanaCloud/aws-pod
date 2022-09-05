25589e6143109507a1eec7c5d07aabf7009ff221
Julio Faerman <jufaerma@redhat.com>
2022-09-05 18:19:45 +0000

    cli changes

--- codebuild-skel.sh
+++ codebuild-skel.sh
@@ -1,1 +1,2 @@
-aws codebuild create-project --cli-input-json file://codebuild-skel.json 
+# aws codebuild create-project --generate-cli-skeleton > codebuild-skel.json 
+# aws codebuild create-project --cli-input-json file://codebuild-skel.json 
