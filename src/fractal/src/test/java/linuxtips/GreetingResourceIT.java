302241bdf358646b0dbc0b46ff51cf7e538a3347
Julio Faerman <jufaerma@redhat.com>
2022-09-10 19:34:17 +0000

    live

---
+++ src/fractal/src/test/java/linuxtips/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package linuxtips;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
