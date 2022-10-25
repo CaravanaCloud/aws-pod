49c9be51f1a140b8f84e192effc1acab07d024b3
Julio Faerman <jufaerma@redhat.com>
2022-10-25 10:51:34 +0200

    GC

--- codebuild-skel.sh
+++
@@ -1,3 +0,0 @@
-# aws codebuild create-project --generate-cli-skeleton > codebuild-skel.new.json 
-# aws codebuild create-project --cli-input-json file://codebuild-skel.json 
-# aws codebuild start-build --project-name multiverse-build-nopipeline
